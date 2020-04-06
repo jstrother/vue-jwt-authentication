750bf25d4fe3646f9a875075331850c5c66d6a7f
Jim Strother <jim.strother@gmail.com>
2020-04-05 18:36:05 -0700

    Added: LoginUser.vue

--- src/store/index.js
+++ src/store/index.js
@@ -21,6 +21,15 @@ export default new Vuex.Store({
         commit('SET_USER_DATA', data);
       });
     },
+    login({ commit }, credentials) {
+      return axios.post('//localhost:3000/login', credentials).then(({ data }) => {
+        commit('SET_USER_DATA', data);
+      });
+    },
   },
-  modules: {},
+  getters: {
+    loggedIn(state) {
+      return !!state.user;
+    },
+  },
 });
