70ce268743dcbca80612ccff74bfe4e17fd06de5
Jim Strother <jim.strother@gmail.com>
2020-04-05 17:32:16 -0700

    Initial Commit && RegisterUser.vue for new users

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/Home.vue';
+import Dashboard from '@/views/Dashboard.vue';
+import RegisterUser from '@/views/RegisterUser';
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  mode: "history",
+const router = new Router({
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser,
+    },
+  ],
 });
 
 export default router;
